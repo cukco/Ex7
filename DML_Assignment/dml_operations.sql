82d9f507ff6c91d29b86f27960fbc0aea1f5bbda
Hưng Đinh <hung.dq2416937@sis.hust.edu.vn>
2026-05-30 18:31:18 +0700

    Delete DML_Assignment directory

--- DML_Assignment/dml_operations.sql
+++
@@ -1,9 +0,0 @@
-update products
-set price=price*1.1 where category='Electronics';
-
-update orders
-set status='CONFIRMED' where status='PENDING';
-
-delete from customers
-where customer_id not in (select customer_id from orders);
-
--- DML_Assignment/queries.sql
+++
@@ -1,5 +0,0 @@
-select city, count(customer_id) from customers
-group by city;
-
-select *from products
-where product_id not in (select product_id from order_details);
